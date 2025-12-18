56acf87ebdb2247add122f40bede937947582adc
wrever <brunomirandaes10@gmail.com>
2025-12-18 18:45:53 -0300

    eliminar node module de repo

--- node_modules/stellar-mcp/src/interfaces/soroban/GetContractMethods.ts
+++
@@ -1,5 +0,0 @@
-import { z } from 'zod';
-
-import { GetContractMethodsSchema } from '../../stellar/soroban/schemas.js';
-
-export type IGetContractMethodsArgs = z.infer<typeof GetContractMethodsSchema>;
